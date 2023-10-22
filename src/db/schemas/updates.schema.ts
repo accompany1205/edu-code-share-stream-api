cf81250e75a850d81396a8c349c4d8293c3114cf
Fabian Koder <fabian.koder@nextglobe.net>
2023-10-22 17:15:12 +0200

    Remove unused "db" files

--- src/db/schemas/updates.schema.ts
+++
@@ -1,8 +0,0 @@
-import { Entity, Schema, Client, Repository } from "redis-om";
-
-class Updates extends Entity {}
-
-export const updates = new Schema(Updates, {
-  roomId: { type: "string" },
-  updates: { type: "string" },
-});
