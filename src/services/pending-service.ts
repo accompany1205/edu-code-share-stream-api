483959e9f90e9e0b252b714af722947b9e6dbc43
Fabian Koder <fabian.koder@nextglobe.net>
2023-11-21 23:50:43 +0100

    Fix

--- src/services/pending-service.ts
+++ src/services/pending-service.ts
@@ -1,30 +1,34 @@
-import { type DocInfo } from "./redis-update.service"
+import { type DocInfo } from "./redis-update.service";
 
 export interface Pending {
-  version: number
-  socketId: string
+  version: number;
+  socketId: string;
 }
 
 interface AddPendingProps extends DocInfo {
-  pending: Pending
+  pending: Pending;
 }
 
 interface SetPendingProps extends DocInfo {
-  pendings: Pending[]
+  pendings: Pending[];
 }
 
 interface IsPendingExistProps extends DocInfo, Pending {}
 
 export class PendingManager {
-  private pending: Record<string, Pending[]>  = {};
+  private pending: Record<string, Pending[]> = {};
 
-  private setPendings = ({ fileName, roomId, pendings }: SetPendingProps): void => {
+  private setPendings = ({
+    fileName,
+    roomId,
+    pendings,
+  }: SetPendingProps): void => {
     this.pending[`${roomId}${fileName.id}`] = pendings;
   };
 
   getPendings = ({ fileName, roomId }: DocInfo): Pending[] => {
     return this.pending[`${roomId}${fileName.id}`] ?? [];
-  }
+  };
 
   add = ({ pending, ...docInfo }: AddPendingProps) => {
     const oldPendings = this.getPendings(docInfo) ?? [];
@@ -32,26 +36,31 @@ export class PendingManager {
     pendings.push(pending);
 
     this.setPendings({ ...docInfo, pendings });
-  }
+  };
 
   pop = (docInfo: DocInfo): Pending => {
-    const pendings= this.getPendings(docInfo);
+    const pendings = this.getPendings(docInfo);
     const removedPending = pendings.pop();
     this.setPendings({ ...docInfo, pendings });
 
-    return removedPending as Pending
-  }
+    return removedPending as Pending;
+  };
 
-  isPendingExist = ({ version: v, socketId: s, ...docInfo }: IsPendingExistProps): boolean => {
+  isPendingExist = ({
+    version: v,
+    socketId: s,
+    ...docInfo
+  }: IsPendingExistProps): boolean => {
     const pendings = this.getPendings(docInfo);
 
-    return pendings.some(({ version, socketId }) =>
-      v === version && s === socketId);
-  }
+    return pendings.some(
+      ({ version, socketId }) => v === version && s === socketId,
+    );
+  };
 
   deletePending = ({ roomId, fileName }: DocInfo) => {
     delete this.pending[`${roomId}${fileName.id}`];
-  }
+  };
 }
 
 export const pendingManager = new PendingManager();
--- src/socket-callbacks/get-document.ts
+++ src/socket-callbacks/get-document.ts
@@ -6,46 +6,47 @@ import { getUniqCursorName } from "../utils/uniq-cursor-name.utils";
 import { SocketEvents, type File } from "./events";
 
 interface GetDocumentProps {
-	roomId: string
-	cursorName: string
-	fileName: File
-	preloadedCode?: string
-  defaultFileName: string
+  roomId: string;
+  cursorName: string;
+  fileName: File;
+  preloadedCode?: string;
+  defaultFileName: string;
 }
 
-export const getDocument = (socket: Socket) => async ({
-  cursorName: _cursorName,
-  roomId,
-  fileName,
-  defaultFileName
-}: GetDocumentProps) => {
-  const cursorName = getUniqCursorName(_cursorName);
+export const getDocument =
+  (socket: Socket) =>
+  async ({
+    cursorName: _cursorName,
+    roomId,
+    fileName,
+    defaultFileName,
+  }: GetDocumentProps) => {
+    const cursorName = getUniqCursorName(_cursorName);
 
-  if (!updateService.isRoomExist(roomId)) {
-    socket.join(roomId);
-  }
+    if (!updateService.isRoomExist(roomId)) {
+      socket.join(roomId);
+    }
 
-  try {
-    const { docUpdates: { updates, doc } } = await updateService.getDocument({
-      roomId,
-      fileName,
-      defaultFileName
-    });
+    try {
+      const {
+        docUpdates: { updates, doc },
+      } = await updateService.getDocument({
+        roomId,
+        fileName,
+        defaultFileName,
+      });
 
-    const docInfo = await updateService.getCodeInfo(roomId);
+      const docInfo = await updateService.getCodeInfo(roomId);
 
-    socket.broadcast.emit(`${SocketEvents.RoomExistResponse}${roomId}`);
-    socket.emit(
-      SocketEvents.GetDocResponse,
-      {
+      socket.broadcast.emit(`${SocketEvents.RoomExistResponse}${roomId}`);
+      socket.emit(SocketEvents.GetDocResponse, {
         version: updates.length,
         doc,
         cursorName,
         updates,
-        docInfo
-      }
-    );
-  } catch (error) {
-    console.log("getDocument", error);
-  }
-}
+        docInfo,
+      });
+    } catch (error) {
+      console.log("getDocument", error);
+    }
+  };
